a2e80e93fae81cc59d7b25c4b6ffedc52bd5d1bd
akshayrangnekar <akshay@annkh.com>
2014-03-16 19:10:24 +0000

    Finished v1 of handrolled pom

---
+++ src/main/java/com/listerly/HelloWorldServlet.java
@@ -0,0 +1,20 @@
+package com.listerly;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class HelloWorldServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, handrolled AppEngine!");
+	}
+
+}
